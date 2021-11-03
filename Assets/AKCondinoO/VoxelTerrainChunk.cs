5652256bb5eb54bedf6c5037153252acfd76dae5
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-11-03 02:33:33 -0300

    ...

--- Assets/AKCondinoO/VoxelTerrainChunk.cs
+++ Assets/AKCondinoO/VoxelTerrainChunk.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using static AKCondinoO.Voxels.VoxelTerrain;
 
 namespace AKCondinoO.Voxels{internal class VoxelTerrainChunk:MonoBehaviour{
@@ -6,4 +8,44 @@ internal const ushort Height=(256);
 internal const ushort Width=(16);
 internal const ushort Depth=(16);
 internal const ushort FlattenOffset=(Width*Depth);
+        
+internal LinkedListNode<VoxelTerrainChunk>expropriated;
+
+#region Rendering
+
+static readonly VertexAttributeDescriptor[]layout=new[]{
+new VertexAttributeDescriptor(VertexAttribute.Position ,VertexAttributeFormat.Float32,3),
+new VertexAttributeDescriptor(VertexAttribute.Normal   ,VertexAttributeFormat.Float32,3),
+new VertexAttributeDescriptor(VertexAttribute.Color    ,VertexAttributeFormat.Float32,4),
+new VertexAttributeDescriptor(VertexAttribute.TexCoord0,VertexAttributeFormat.Float32,2),
+new VertexAttributeDescriptor(VertexAttribute.TexCoord1,VertexAttributeFormat.Float32,2),
+new VertexAttributeDescriptor(VertexAttribute.TexCoord2,VertexAttributeFormat.Float32,2),
+new VertexAttributeDescriptor(VertexAttribute.TexCoord3,VertexAttributeFormat.Float32,2),
+};
+
+MeshUpdateFlags meshFlags=MeshUpdateFlags.DontValidateIndices|MeshUpdateFlags.DontNotifyMeshUsers|MeshUpdateFlags.DontRecalculateBounds;
+        
+internal Mesh mesh;
+
+#endregion
+
+internal Bounds worldBounds;
+        
+internal new MeshRenderer renderer;
+internal new MeshCollider collider;
+
+internal readonly MarchingCubesBackgroundContainer marchingCubesBG=new MarchingCubesBackgroundContainer();
+internal class MarchingCubesBackgroundContainer:BackgroundContainer{
+}
+internal class MarchingCubesMultithreaded:BaseMultithreaded<MarchingCubesBackgroundContainer>{
+protected override void Execute(){
+}
+}
+
+void Awake(){
+ mesh=new Mesh(){
+  bounds=worldBounds=new Bounds(Vector3.zero,new Vector3(Width,Height,Depth)),
+ };
+}
+
 }}
