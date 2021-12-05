0572fd722e7b4541bd5e02e3e3557bd68c1a9f94
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2021-12-05 03:44:48 -0300

    ...

---
+++ Assets/AKCondinoO/MainCamera.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AKCondinoO{internal class MainCamera:MonoBehaviour{
+
+// Update is called once per frame
+void Update(){         
+}
+
+}}
